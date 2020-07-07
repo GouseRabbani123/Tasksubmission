6f1b5845a55751c9bc92677c1861fd65ee052434
GouseRabbani123 <67941879+GouseRabbani123@users.noreply.github.com>
2020-07-07 10:35:13 +0530

    Task upload_1

---
+++ scripts.js
@@ -0,0 +1,12 @@
+//Task1
+
+//Task 1: Simple Programs todo for variables
+//Declare four variables without assigning values and print them in console
+var var1;
+var var2;
+var var3;
+var var4;
+
+//How to get value of the variable myvar as output
+var myvar= 1;
+console.log(myvar);
